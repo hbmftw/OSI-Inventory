9e80bf6b636ca22f5a3df0838208b7bf02d5fcc4
RyanPowellBeard <140354287+RyanPowellBeard@users.noreply.github.com>
2026-01-01 13:26:50 -0700

    Fixed sync issues and added updates

---
+++ src/db/data_connect.java
@@ -0,0 +1,33 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class data_connect {
+
+    Connection conn = null;
+
+    public void connectDB() {
+        String url = "";
+        String user = "";
+        String password = "";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+    }
+    public void closeDB() throws SQLException {
+        conn.close();
+    }
+
+
+}
